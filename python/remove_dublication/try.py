16568df596b1af28ec46ccbddf184eca5254ff20
Abukiya <yadaman36@gmail.com>
2026-06-14 14:14:43 +0300

    solved with the hlep of cloud(at some point i think it ok to get a hlelp)

--- python/remove_dublication/try.py
+++ python/remove_dublication/try.py
@@ -1,12 +1,11 @@
 nums = [1,1,2]
 new_num =[]
 
-for i in range(len(nums)):
-    check =nums[i]
-    for i in range(i+1,len(nums)):
-        if check == nums[i]:
-            continue
-        else:
-            new_num.append(check)
-print(check)
-print(new_num)
+def removeDuplicates(nums):
+    slow = 0
+    for fast in range(1, len(nums)):
+        if nums[fast] != nums[slow]:
+            slow += 1
+            nums[slow] = nums[fast]
+    return slow + 1
+print(removeDuplicates(nums))
