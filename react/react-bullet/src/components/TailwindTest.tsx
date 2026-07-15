f53b8d14c7fa00f80910de330d4c545592af540f
Abukiya <yadaman36@gmail.com>
2026-07-15 20:02:20 +0300

    feat: integrate Tailwind CSS and remove legacy styles

---
+++ react/react-bullet/src/components/TailwindTest.tsx
@@ -0,0 +1,9 @@
+
+export default function TailwindTest() {
+  return (
+    <button className="w-48 h-48 bg-blue-500 hover:bg-blue-700 text-red-500 font-bold py-2 px-4 rounded hover:scale-400">
+      Click Me
+    </button>
+
+  )
+}
